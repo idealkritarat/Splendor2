414a1bbbd9ae4405d0ffb48eeb2bab5d181bb9ab
Kritarat.M <ideal141148@gmail.com>
2024-11-30 17:05:05 +0700

    fix: optional number of players

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,14 @@
-var number_of_player = 2
+let number_of_player;
+
+do {
+    number_of_player = parseInt(prompt("Please select the number of players (1-4):"), 10);
+
+    if (isNaN(number_of_player) || number_of_player < 1 || number_of_player > 4) {
+        alert("Invalid input! Please enter a number between 1 and 4.");
+        number_of_player = null;
+    }
+} while (!number_of_player);
+
 let player_turn = -1;
 
 function update_turn(){
